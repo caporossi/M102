fd80d57b6c36c1b5445a0487edefceea12fe6b3a
Stas <stasel@gmail.com>
2014-03-24 00:01:36 +0200

    Fix for homework 4.3

--- week4/homework 4.3.js
+++ week4/homework 4.3.js
@@ -1,6 +1,3 @@
-rs.add("127.0.0.1:27002");
-rs.add("127.0.0.1:27003");
-
 var cfg = rs.config();
 cfg.members.push({_id:1, host:"Stass-MacBook-Air.local:27002"})
 cfg.members.push({_id:2, host:"Stass-MacBook-Air.local:27003"})
